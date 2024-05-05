6a4d5b9d65faf22f44699ca57bbf2c2116eb46cd
munnukumar <rvsingh50g@gmail.com>
2024-05-05 05:56:48 +0530

    Dynamic Routes Part 2( Post ) and add cart model

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
-
+router.post('/cart', shopController.postCart)
 
 router.get('/cart', shopController.getCart);
 
