adad659199d2e87ef64c0404440a37aa9dd1c6fe
munnukumar <rvsingh50g@gmail.com>
2024-06-16 23:55:43 +0530

    created user model and add to cart

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,13 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById('666f24452508fa5c70d8a25f')
+  .then(user => {
+    req.user = new User(user.name, user.email, user.cart, user._id);
+    next();
+  })
+  .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
