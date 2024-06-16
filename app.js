c5362c88a80ddc70fe94de4abb8b4fa323feba8e
munnukumar <rvsingh50g@gmail.com>
2024-06-17 01:44:27 +0530

    implement all functionality with mongoose

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -20,11 +20,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   User.findById('666f24452508fa5c70d8a25f')
-  .then(user => {
-    req.user = new User(user.name, user.email, user.cart, user._id);
-    next();
-  })
-  .catch(err => console.log(err));
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+
+
 });
 
 app.use('/admin', adminRoutes);
@@ -32,6 +34,24 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose.connect("mongodb://localhost:27017/shopApp", { useNewUrlParser: true })
+  .then(result => {
+    console.log("connected to mongoDb")
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Munnu kumar',
+          email: 'munnu@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+    console.log("server is listing on PORT 3000")
+  })
+  .catch(err => {
+    console.log(err);
+  });
